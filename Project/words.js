000b498ebb7331b2f5b9ef4d8d2ecd401fafd22b
Sishir Kharel <87405061+sishir123@users.noreply.github.com>
2023-12-21 15:41:43 +0000

    Add files via upload

---
+++ Project/words.js
@@ -0,0 +1,114 @@
+const words = [
+    {
+        word: "addition",
+        hint: "The process of adding numbers"
+    },
+    {
+        word: "meeting",
+        hint: "Event in people gather together"
+    },
+    {
+        word: "number",
+        hint: "Maths Symbol Used for counting"
+    },
+    {
+        word: "exchange",
+        hint: "The act of trading"
+    },
+    {
+        word: "canvas",
+        hint: "Piece of fabric for oil painting"
+    },
+    {
+        word: "pocket",
+        hint: "A bag of carrying small items"
+    },
+    {
+        word: "dinner",
+        hint: "Main meal of the day"
+    },
+    {
+        word: "tourist",
+        hint: "A Person who is visiting new places"
+    },
+    {
+        word: "clothes",
+        hint: "Items worn to cover the body"
+    },
+    {
+        word: "health",
+        hint: "The state of being free from illness"
+    },
+    {
+        word: "Curious",
+        hint: "Eager to know or learn something"
+    },
+    {
+        word: "touch",
+        hint: "Come into or be in contact with"
+    },
+    {
+        word: "receipt",
+        hint: "Mark(a bill) as paid"
+    },
+    {
+        word: "public",
+        hint: "Of or concerning the people as a whole"
+    },
+    {
+        word: "troop",
+        hint: "A soldiers or armed forces"
+    },
+    {
+        word: "donate",
+        hint: "Give (money or goods) for a good cause"
+    }, 
+    {
+        word: "request",
+        hint: "An act of asking politely for something"
+    },   
+    {
+        word: "Noise",
+        hint: "A sound, especially one that is loud"
+    },
+    {
+        word: "Design",
+        hint: "A decorative pattern."
+    },
+    {
+        word: "Temple",
+        hint: "A building devoted to the worship of a god or gods."
+    },
+    {
+        word: "Ocean",
+        hint: "A very large expanse of sea."
+    },
+    {
+        word: "Expense",
+        hint: "The cost incurred in or required for something."
+    },
+    {
+        word: "Dustbin",
+        hint: "A container for household refuse, especially one kept outside."
+    },
+    {
+        word: "Decade",
+        hint: "A period of ten years."
+    },
+    {
+        word: "Camera",
+        hint: "a device for recording visual images in the form of photographs, or video signals. "
+    },
+    {
+        word: "Factory",
+        hint: "A building or group of buildings where goods are manufactured"
+    },
+    {
+        word: "Statue",
+        hint: "A cast figure of a person especially one that is life-size or larger."
+    },
+    {
+        word: "Farmer",
+        hint: "A person who owns or manages a farm."
+    },
+]
